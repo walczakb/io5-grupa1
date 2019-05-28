5bbb438c17c6ebb42423a03efb3685ed9fd61014
z1126923 <artur.salawa789@gmail.com>
2019-05-28 09:37:49 +0200

    first

--- src/main/java/io/db/DbEngine.java
+++ src/main/java/io/db/DbEngine.java
@@ -4,7 +4,7 @@ public interface DbEngine {
     interface ItemListBuilder {
         void add(int id, String name, int count, int price);
     }
-
+         
     void readItems(ItemListBuilder builder);
     int insertItem(String name, int count, int price);
     void updateItem(int id, String name, int count, int price);
